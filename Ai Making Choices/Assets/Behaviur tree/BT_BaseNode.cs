44a63eb92487a40b6a7b66d0212bbce49101c077
GioTheGreen <B1050622@live.tees.ac.uk>
2024-02-07 00:08:04 +0000

    node testing

--- Ai Making Choices/Assets/Behaviur tree/BT_BaseNode.cs
+++ Ai Making Choices/Assets/Behaviur tree/BT_BaseNode.cs
@@ -5,12 +5,14 @@ using XNode;
 
 public class BT_BaseNode : Node {
 
+    [Input] public float value;
+    [Output] public float result;
+	
+
 	// Use this for initialization
 	protected override void Init() {
 		base.Init();
-		
 	}
-
 	// Return the correct value of an output port when requested
 	public override object GetValue(NodePort port) {
 		return null; // Replace this
