8915db9d69a8da31bc325dd2184e4022ecd2e199
GioTheGreen <B1050622@live.tees.ac.uk>
2024-03-04 13:09:24 +0000

    update

--- Ai Making Choices/Assets/Behaviur tree/NG_BehaivurTree.cs
+++ Ai Making Choices/Assets/Behaviur tree/NG_BehaivurTree.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using XNode;
 
 [CreateAssetMenu]
 public class NG_BehaivurTree : NodeGraph {
 	public BT_BaseNode curent;
+    public enum EState
+    {
+        eUnkown,
+        eInProgress,
+        eFailed,
+        eSuccess
+    }
+
 
     public void Refreash()
     {
@@ -23,5 +33,53 @@ public class NG_BehaivurTree : NodeGraph {
         }
     }
 
+    public BT_BaseNode NextNode()
+    {return null;
 
+        foreach (NodePort p in curent.Ports)
+        {
+            if (p.fieldName == "exit")
+            {
+                if (p.ConnectionCount > 0)
+                {
+                    NodePort[] conections = p.GetConnections().ToArray();
+                    BT_BaseNode[] childNodes = new BT_BaseNode[conections.Length];
+                    for (int i = 0; i < conections.Length; i++)
+                    {
+                        childNodes[i] = conections[i].Connection.node as BT_BaseNode;     //get all child nodes
+                    }
+
+                    
+
+                }
+            }
+        }
+    }
+
+    private BT_BaseNode[] InOrder(BT_BaseNode[] nodes)
+    {
+        BT_BaseNode[] toReturn = new BT_BaseNode[0];
+
+        while (nodes.Length > 0)
+        {
+            int currnetHighest = 0;
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (nodes[i].position.y < nodes[currnetHighest].position.y)
+                {
+                    currnetHighest = i;
+                }
+            }
+
+            Array.Resize(ref toReturn, toReturn.Length + 1);
+            toReturn[toReturn.Length] = nodes[currnetHighest];
+
+            for (int i = currnetHighest; i < nodes.Length - 1; i++)
+            {
+                nodes[i] = nodes[i + 1];
+            }
+            Array.Resize(ref nodes, toReturn.Length - 1);
+        }
+        return toReturn;
+    }
 }
